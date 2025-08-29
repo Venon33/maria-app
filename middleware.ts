f9c31c8dc8a72e77131802d99988c5ed313c8cb7
Venon33 <116621878+Venon33@users.noreply.github.com>
2025-08-29 19:45:13 +0200

    Contador visitas: cookie 7 días

--- middleware.ts
+++ middleware.ts
@@ -12,29 +12,18 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
   const today = new Date().toISOString().slice(0, 10); // YYYY-MM-DD
-  const cookie = req.cookies.get("vdate");
-
-  // Si ya contamos hoy desde este dispositivo, no sumamos
-  if (cookie?.value === today) return res;
 
   try {
     await redis.incr("visitas:total");
     await redis.incr(`visitas:byday:${today}`);
   } catch (e) {
-    console.error("Error incrementando visitas:", e);
+    console.error("❌ Error incrementando visitas:", e);
   }
-
-  // Guardamos la fecha de hoy en la cookie (expira en 2 días por seguridad)
-  res.cookies.set("vdate", today, {
-    maxAge: 60 * 60 * 1, // 1 hora para agregar dias *1, otro *
-    sameSite: "lax",
-    secure: true,
-    path: "/",
-  });
 
   return res;
 }
 
-// Cuenta solo en la home (ajusta si quieres)
+// Solo contamos visitas en la home (ajusta si quieres en más rutas)
 export const config = { matcher: ["/"] };
+
 
